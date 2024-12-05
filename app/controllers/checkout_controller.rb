1a9d2df8db3ee1fca80e86d037a274aaeef3b634
archiegabani <archie.gabani@gmail.com>
2024-12-05 01:55:30 -0600

    Tried to Fix error in checkout.

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -21,21 +21,7 @@ class CheckoutController < ApplicationController
             end
 
             # Calculate taxes and update total price
-            gst_rate = current_user.province.gst_rate
-            pst_rate = current_user.province.pst_rate
-            gst = @order.total_price * gst_rate
-            pst = @order.total_price * pst_rate
-            total_with_tax = (@order.total_price + gst + pst).round(2)
-
-            @order.update!(status: 'completed', total_price: total_with_tax)
-            puts "✅ Order successfully placed!"
-            flash[:notice] = 'Order successfully placed!'
-            redirect_to orders_path
-          end
-        else
-          puts "❌ No pending order found!"
-          redirect_to cart_path
-        end
+            
     rescue ActiveRecord::RecordInvalid => e
         flash[:error] = "Checkout failed: #{e.message}"
         redirect_to checkout_path
